94e16006894fa60a0d74367246658b9347cb2a65
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2024-05-18 21:21:48 +0200

    Add view example
    Add view example in HTML

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// const body = document.querySelector(".body");
+// const elem = document.querySelector(".ul-1").firstElementChild;
+
+// console.log(body.previousElementSibling);
+// console.log(elem.classList);
