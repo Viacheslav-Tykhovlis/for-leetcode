ea197f187a510c8208c8ff6b56912fba46984beb
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2024-05-19 16:21:42 +0200

    Change task 121
    Add table to task 121

--- js/task-121.js
+++ js/task-121.js
@@ -7,6 +7,7 @@
  * @return {number}
  */
 
+// solution
 var maxProfit = function (prices) {
   let buy = prices[0];
   let maxProfit = 0;
@@ -20,6 +21,38 @@ var maxProfit = function (prices) {
   }
   return maxProfit;
 };
+
+// imports
+import { quantity, exampleDate, userData } from "../index.js";
+
+// name inputs
+quantity(1, ["prices"]);
+
+// Examples
+// Example 1
+let example = [[7, 1, 5, 3, 6, 4]];
+exampleDate(1, 1, example, maxProfit);
+
+// Example 2
+example = [[7, 6, 4, 3, 1]];
+exampleDate(2, 1, example, maxProfit);
+
+// Example 3
+example = [[2, 4, 1]];
+exampleDate(3, 1, example, maxProfit);
+
+// You can enter your data for verification
+const result4 = document.querySelector(`#ex45`);
+const result5 = document.querySelector(`#ex55`);
+
+result4.addEventListener(
+  "click",
+  () => (result4.value = userData(4, 1, maxProfit))
+);
+result5.addEventListener(
+  "click",
+  () => (result5.value = userData(5, 1, maxProfit))
+);
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
 console.log(maxProfit([7, 6, 4, 3, 1]));
