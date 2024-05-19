9f8147ca7d78a8bc4ba1394cc73daf2928bcb5bd
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2024-05-19 16:35:50 +0200

    change task 58
    Add table to task 58

--- js/task-58.js
+++ js/task-58.js
@@ -11,7 +11,7 @@ var lengthOfLastWord = function (s) {
 
   if (s.trim().includes(" ")) {
     const arr = s.split(" ");
-    console.log("arr: ", arr);
+    // console.log("arr: ", arr);
 
     for (let i = arr.length - 1; i > 0; i--) {
       if (arr[i] !== "") {
@@ -23,11 +23,43 @@ var lengthOfLastWord = function (s) {
     lastWord = s.trim();
   }
 
-  console.log("lastWord: ", lastWord);
+  // console.log("lastWord: ", lastWord);
   const result = lastWord.length;
-  console.log("result: ", result);
+  // console.log("result: ", result);
   return result;
 };
+
+// imports
+import { quantity, exampleDate, userData } from "../index.js";
+
+// name inputs
+quantity(1, ["s"]);
+
+// Examples
+// Example 1
+let example = ["Hello World"];
+exampleDate(1, 1, example, lengthOfLastWord);
+
+// Example 2
+example = ["   fly me   to   the moon  "];
+exampleDate(2, 1, example, lengthOfLastWord);
+
+// Example 3
+example = ["a"];
+exampleDate(3, 1, example, lengthOfLastWord);
+
+// You can enter your data for verification
+const result4 = document.querySelector(`#ex45`);
+const result5 = document.querySelector(`#ex55`);
+
+result4.addEventListener(
+  "click",
+  () => (result4.value = userData(4, 1, lengthOfLastWord))
+);
+result5.addEventListener(
+  "click",
+  () => (result5.value = userData(5, 1, lengthOfLastWord))
+);
 
 console.log(lengthOfLastWord("Hello World"));
 console.log(lengthOfLastWord("   fly me   to   the moon  "));
